ccd1cb4f04f3ec41b865a5fa8edfebfd5fc78a9a
vasivuk <vasivuk@yahoo.com>
2022-03-19 11:12:21 +0100

    Dodata kompletna dokumentacija za klasu AutoGuma

--- src/auto_radnja/gume/AutoGuma.java
+++ src/auto_radnja/gume/AutoGuma.java
@@ -1,14 +1,41 @@
 package auto_radnja.gume;
 
+/**
+ * Predstavlja automobilsku gumu sa svim svojim dimenzijama i modelom.
+ * 
+ * @author Vale
+ *
+ */
+
 public class AutoGuma {
+	/**
+	 * Marka i model gume.
+	 */
 	private String markaModel = null;
+	/**
+	 * Prečnik gume izražen u centimetrima.
+	 */
 	private int precnik = -1;
+	/**
+	 * Širina gume izražena u milimetrima.
+	 */
 	private int sirina = -1;
+	/**
+	 * Visina gume izražena u centimetrima.
+	 */
 	private int visina = -1;
 
 	public AutoGuma() {
 	}
 
+	/**
+	 * Postavlja marku i model, prečnik, širinu i visinu gume na zadate vrednosti
+	 * 
+	 * @param markaModel nova marka i model
+	 * @param precnik    novi prečnik
+	 * @param sirina     nova širina
+	 * @param visina     nova visina
+	 */
 	public AutoGuma(String markaModel, int precnik, int sirina, int visina) {
 		super();
 		setMarkaModel(markaModel);
@@ -17,10 +44,23 @@ public class AutoGuma {
 		setVisina(visina);
 	}
 
+	/**
+	 * Vraća marku i model gume.
+	 * 
+	 * @return marka i model kao String
+	 */
 	public String getMarkaModel() {
 		return markaModel;
 	}
 
+	/**
+	 * Postavlja novu vrednost za atribut markaModel.
+	 * 
+	 * @param markaModel nova marka i model gume
+	 * @throws java.lang.NullPointerException     ako su uneti marka i model null
+	 * @throws java.lang.IllegalArgumentException ako uneti marka i model sadrže
+	 *                                            manje od 3 znaka
+	 */
 	public void setMarkaModel(String markaModel) {
 		if (markaModel == null)
 			throw new NullPointerException("Morate uneti marku i model");
@@ -29,42 +69,90 @@ public class AutoGuma {
 		this.markaModel = markaModel;
 	}
 
+	/**
+	 * Vraća prečnik gume.
+	 * 
+	 * @return prečnik gume u centimetrima
+	 */
 	public int getPrecnik() {
 		return precnik;
 	}
 
+	/**
+	 * Postavlja novu vrednost za atribut prečnik.
+	 * 
+	 * @param precnik novi prečnik gume
+	 * @throws java.lang.IllegalArgumentException ako je prečnik manji od 13cm ili
+	 *                                            veći od 22cm
+	 */
 	public void setPrecnik(int precnik) {
 		if (precnik < 13 && precnik > 22)
 			throw new IllegalArgumentException("Precnik van opsega");
 		this.precnik = precnik;
 	}
 
+	/**
+	 * Vraća širinu gume.
+	 * 
+	 * @return širina gume izrazena u milimetrima
+	 */
 	public int getSirina() {
 		return sirina;
 	}
 
+	/**
+	 * Postavlja novu vrednost za širinu gume
+	 * 
+	 * @param sirina nova širina gume
+	 */
 	public void setSirina(int sirina) {
 		if (sirina < 135 && sirina > 355)
 			throw new IllegalArgumentException("Sirina van opsega");
 		this.sirina = sirina;
 	}
 
+	/**
+	 * Vraća visinu gume.
+	 * 
+	 * @return visina gume izražena u centimetrima
+	 */
 	public int getVisina() {
 		return visina;
 	}
 
+	/**
+	 * Postavlja novu vrednost za visinu gume.
+	 * 
+	 * @param visina nova visina gume
+	 * @throws java.lang.IllegalArgumentException ako visina nije veća od 25cm ili
+	 *                                            nije manja od 95cm
+	 */
 	public void setVisina(int visina) {
 		if (visina < 25 || visina > 95)
 			throw new IllegalArgumentException("Visina van opsega");
 		this.visina = visina;
 	}
 
+	/**
+	 * Vraća sve podatke o gumi u jednom Stringu.
+	 * 
+	 * @return String sa svim podacima o gumi.
+	 */
 	@Override
 	public String toString() {
 		return "AutoGuma [markaModel=" + markaModel + ", precnik=" + precnik + ", sirina=" + sirina + ", visina="
 				+ visina + "]";
 	}
 
+	/**
+	 * Poredi dve gume po marki, modelu i po dimenzijama
+	 * 
+	 * @return
+	 *         <ul>
+	 *         <li>true - ako su marka model i sve dimenzije iste kod obe gume</li>
+	 *         <li>false - ako to nije slucaj</li>
+	 *         </ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
